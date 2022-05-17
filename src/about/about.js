10d687260ecfb77e9395a29d70d17114eb7e029b
mijack <dr.mijack@gmail.com>
2022-05-17 13:38:16 +0300

    changed some stuff

--- src/about/about.js
+++ src/about/about.js
@@ -1,108 +1,104 @@
-import './about.css'
+import "./about.css";
 
-const about = document.createElement('div');
-const aboutTitle = document.createElement('span');
-const aboutWrapper =document.createElement('div');
-const aboutContent =document.createElement('div');
-const lucisMap = document.createElement('div');
-const paragraph = document.createElement('p');
-const Footer = document.createElement('footer');
-
+const about = document.createElement("div");
+const aboutTitle = document.createElement("span");
+const aboutWrapper = document.createElement("div");
+const aboutContent = document.createElement("div");
+const lucisMap = document.createElement("div");
+const paragraph = document.createElement("p");
+const Footer = document.createElement("footer");
 
 //add class
-aboutTitle.classList.toggle('about-title');
-aboutWrapper.classList.toggle('about-wrapper');
-aboutContent.classList.toggle('about');
-lucisMap.classList.toggle('location-map');
-paragraph.classList.toggle('p-about');
-about.classList.toggle('about-container');
+aboutTitle.classList.toggle("about-title");
+aboutWrapper.classList.toggle("about-wrapper");
+aboutContent.classList.toggle("about");
+lucisMap.classList.toggle("location-map");
+paragraph.classList.toggle("p-about");
+about.classList.toggle("about-container");
 //append item
 aboutTitle.textContent = "Map of Lucis";
-Footer.innerHTML =`<span> Created by  <a href="https://github.com/MijackK/restuarant/tree/main/src"><i class="fab fa-github"></i></a></span>`;
+Footer.innerHTML = `<span> Created by  <a href="https://github.com/MijackK/restuarant/tree/main/src"><i class="fab fa-github"></i></a></span>`;
 
+const constructAbout = (() => {
+  let content = [
+    `Stunning seaside getaway along the Vannath Coast in southern Leide. Known not only for its world-class spa, but also for the exquisite seafood dishes crafted by Mother of Pearl's chef de cuisine, Coctura Arlund.`,
+    `While the quay currently enjoys its reputation as the kingdom's premier resort, Galdin itself was once a quiet fishing town. That all changed when Niflheim made inroads into Lucian territory some 150 years ago and effectively put all lands outside the Crown City under imperial control. From that time onward, the former fishing village turned into a trading post connecting the Lucian continent with the imperial province of Accordo.`,
+    `In time, the empire shifted their shipping operations from the sea to the skies, and Galdin's importance as an imperial outpost diminished. What the town lost in strategic value, however, it gained in culture, gleaning heavily from Altissian immigrants drawn to Lucian shores. In that sense, one could say the quay owes its current success to the empire's intervention.`,
+  ];
 
-
-const constructAbout = (() =>{
-    let content = [`Stunning seaside getaway along the Vannath Coast in southern Leide. Known not only for its world-class spa, but also for the exquisite seafood dishes crafted by Mother of Pearl's chef de cuisine, Coctura Arlund.`
-    ,`While the quay currently enjoys its reputation as the kingdom's premier resort, Galdin itself was once a quiet fishing town. That all changed when Niflheim made inroads into Lucian territory some 150 years ago and effectively put all lands outside the Crown City under imperial control. From that time onward, the former fishing village turned into a trading post connecting the Lucian continent with the imperial province of Accordo.`
-    ,`In time, the empire shifted their shipping operations from the sea to the skies, and Galdin's importance as an imperial outpost diminished. What the town lost in strategic value, however, it gained in culture, gleaning heavily from Altissian immigrants drawn to Lucian shores. In that sense, one could say the quay owes its current success to the empire's intervention.`];
-
-   for(let i=0;i <content.length;i++){
-       paragraph.textContent =content[i];
-       aboutContent.appendChild(paragraph.cloneNode(true))
-   }
-aboutWrapper.appendChild(aboutContent);
-aboutWrapper.appendChild(lucisMap);
-about.appendChild(aboutTitle);
-about.appendChild(aboutWrapper);
-about.appendChild(Footer);
-
-
-
-
+  for (let i = 0; i < content.length; i++) {
+    paragraph.textContent = content[i];
+    aboutContent.appendChild(paragraph.cloneNode(true));
+  }
+  aboutWrapper.appendChild(aboutContent);
+  aboutWrapper.appendChild(lucisMap);
+  about.appendChild(aboutTitle);
+  about.appendChild(aboutWrapper);
+  about.appendChild(Footer);
 })();
-const mapMovement = (() =>{
-    const map = lucisMap;
-    let shouldMove = false;
-    let coordinates = {x:0,y:0};// cordinates when first clicked, changes by mouse move
-    let mapPosition = {x:-3000,y:-2000}; // starting point
-    let movement ={x:0,y:0}// used to store where the map moves.
-    let offsetX;
-    let offsetY;
-    const startMovement = (e) =>{
-        if(!shouldMove)
-        return
-        offsetX = [0,1,2,3,-1,-2,-3].map(x => x+coordinates.x);// prevent movement from slight offsets
-        offsetY = [0,1,2,3,-1,-2,-3].map(y => y+coordinates.y);// prevent movement from slight offsets
-       
-        movement.x = offsetX.includes(e.clientX) ? 0: e.clientX > coordinates.x ? 12:-12;
-        movement.y =  offsetY.includes(e.clientY) ? 0:e.clientY > coordinates.y ? 12:-12;
-        mapPosition.x = mapPosition.x + movement.x;
-        mapPosition.y = mapPosition.y + movement.y;
-       map.style.backgroundPosition = `${mapPosition.x}px ${mapPosition.y}px`;
-       coordinates.x=e.clientX;
-       coordinates.y =e.clientY;
-    }
-  
-    map.addEventListener('mousedown', e =>{
-        shouldMove=true;
-        e.target.style.cursor = 'grabbing';
-        coordinates.x=e.clientX;
-        coordinates.y =e.clientY;
-    })
+const mapMovement = (() => {
+  const map = lucisMap;
+  let shouldMove = false;
+  let coordinates = { x: 0, y: 0 }; // cordinates when first clicked, changes by mouse move
+  let mapPosition = { x: -3000, y: -2000 }; // starting point
+  let movement = { x: 0, y: 0 }; // used to store where the map moves.
+  let offsetX;
+  let offsetY;
+  const startMovement = (e) => {
+    if (!shouldMove) return;
+    offsetX = [0, 1, 2, 3, -1, -2, -3].map((x) => x + coordinates.x); // prevent movement from slight offsets
+    offsetY = [0, 1, 2, 3, -1, -2, -3].map((y) => y + coordinates.y); // prevent movement from slight offsets
 
-    map.addEventListener('mouseup', e =>{
-        shouldMove=false;
-        e.target.style.cursor = 'grab';
-    })
-  
-    map.addEventListener('mousemove', e =>{
-        startMovement(e);
-      
-    })
+    movement.x = offsetX.includes(e.clientX)
+      ? 0
+      : e.clientX > coordinates.x
+      ? 12
+      : -12;
+    movement.y = offsetY.includes(e.clientY)
+      ? 0
+      : e.clientY > coordinates.y
+      ? 12
+      : -12;
+    mapPosition.x = mapPosition.x + movement.x;
+    mapPosition.y = mapPosition.y + movement.y;
+    map.style.backgroundPosition = `${mapPosition.x}px ${mapPosition.y}px`;
+    coordinates.x = e.clientX;
+    coordinates.y = e.clientY;
+  };
 
-    map.addEventListener('touchmove', e =>{
-        e.preventDefault();
-        startMovement(e.changedTouches[0]);
-    })
+  map.addEventListener("mousedown", (e) => {
+    shouldMove = true;
+    e.target.style.cursor = "grabbing";
+    coordinates.x = e.clientX;
+    coordinates.y = e.clientY;
+  });
 
-    map.addEventListener('touchstart', e =>{
-        shouldMove=true;
-        console.log('start');
-    })
-    map.addEventListener('touchend', e =>{
-        shouldMove=false;
-        console.log('end');
-    })
+  map.addEventListener("mouseup", (e) => {
+    shouldMove = false;
+    e.target.style.cursor = "grab";
+  });
 
-   
+  map.addEventListener("mousemove", (e) => {
+    startMovement(e);
+  });
 
+  map.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+    startMovement(e.changedTouches[0]);
+  });
+
+  map.addEventListener("touchstart", (e) => {
+    shouldMove = true;
+    console.log("start");
+  });
+  map.addEventListener("touchend", (e) => {
+    shouldMove = false;
+    console.log("end");
+  });
 })();
 
-const getAbout = () =>{
-    return about;
-}
+const getAbout = () => {
+  return about;
+};
 
-export{getAbout}
-   
-  
+export { getAbout };
--- src/home/home.js
+++ src/home/home.js
@@ -1,174 +1,193 @@
-import './home.css';
-import myLogo from './logo2.png'
+import "./home.css";
+import myLogo from "./logo2.png";
 //let position =[];
 //document.querySelector('body').addEventListener('click',e => {
-   // position.push({x:e.clientX,y:e.clientY});
-    //console.log(position);
+// position.push({x:e.clientX,y:e.clientY});
+//console.log(position);
 //})
 
+const Home = document.createElement("div");
+const constructHome = (() => {
+  const firstSection = document.createElement("section");
+  const SecondSection = document.createElement("section");
+  const ThirdSection = document.createElement("section");
+  const galdinVideo = document.createElement("img");
+  const firstTextBox = document.createElement("div");
+  const movingText = document.createElement("div");
+  const title1 = document.createElement("span");
+  const subTitle1 = document.createElement("span");
+  const title2 = document.createElement("span");
+  const subTitle2 = document.createElement("span");
+  const title3 = document.createElement("span");
+  const letter = document.createElement("div");
+  const firstWord = document.createElement("div"); // for moving text
+  const secondWord = document.createElement("div"); // for moving text
+  const logo = document.createElement("img");
+  const paragraph1 = document.createElement("p");
+  const paragraph2 = document.createElement("p");
+  const wrapper1 = document.createElement("div");
+  const imageVisible = document.createElement("div");
+  const imageInvisible = document.createElement("div");
 
+  //add class
+  Home.classList.toggle("Home");
+  firstSection.classList.toggle("first-section");
+  SecondSection.classList.toggle("second-section");
+  ThirdSection.classList.toggle("third-section");
+  galdinVideo.classList.toggle("galdin-video");
+  firstTextBox.classList.toggle("first-text");
+  movingText.classList.toggle("moving-text");
+  firstWord.classList.toggle("first-word");
+  secondWord.classList.toggle("second-word");
+  logo.classList.toggle("logo2");
+  title3.classList.toggle("title3");
+  wrapper1.classList.toggle("wrapper1");
+  imageVisible.classList.toggle("img-visible");
+  imageInvisible.classList.toggle("img-invisible");
 
+  //append item
+  Home.appendChild(firstSection);
+  firstSection.appendChild(firstTextBox);
+  firstSection.appendChild(galdinVideo);
+  firstTextBox.appendChild(title1);
+  firstTextBox.appendChild(subTitle1);
+  firstTextBox.appendChild(movingText);
+  movingText.appendChild(firstWord);
+  movingText.appendChild(secondWord);
+  title1.textContent = "The Galdin Quay";
+  subTitle1.textContent = "Ignis Scientia";
+  Home.appendChild(SecondSection);
+  SecondSection.appendChild(logo);
+  SecondSection.appendChild(title2);
+  SecondSection.appendChild(subTitle2);
+  title2.textContent = "16 YEARS";
+  Home.appendChild(ThirdSection);
+  ThirdSection.appendChild(title3);
+  ThirdSection.appendChild(paragraph1);
+  ThirdSection.appendChild(paragraph2);
+  ThirdSection.appendChild(wrapper1);
+  wrapper1.appendChild(imageVisible);
+  imageVisible.appendChild(imageInvisible);
+  subTitle2.textContent =
+    "Final Fantasy XV is an action role-playing game developed and published by Square Enix. The fifteenth main installment of the Final Fantasy series";
+  title3.textContent = " A First-Class Experience";
+  paragraph1.textContent = `A Stunning seaside getaway along the Vannath Coast in southern Leide. Known not only for its world-class spa, but also for the exquisite seafood dishes crafted by Mother of Pearl's chef de cuisine, Coctura Arlund.`;
+  paragraph2.textContent = ` With the sea right at our door we offer only the freshest sea food and if your feeling up to it, we'll let you catch you're own fish doubling the experience`;
 
-const Home = document.createElement('div');
-const constructHome = (() =>{ 
-    const firstSection = document.createElement('section');
-    const SecondSection = document.createElement('section');
-    const ThirdSection = document.createElement('section');
-    const galdinVideo = document.createElement('img');
-    const firstTextBox =document.createElement('div');
-    const movingText = document.createElement('div');
-    const title1 = document.createElement('span');
-    const subTitle1 = document.createElement('span');
-    const title2 = document.createElement('span');
-    const subTitle2 = document.createElement('span');
-    const title3 = document.createElement('span');
-    const letter = document.createElement('div');
-    const firstWord =document.createElement('div'); // for moving text
-    const secondWord =document.createElement('div'); // for moving text
-    const logo = document.createElement('img');
-    const paragraph1 = document.createElement('p');
-    const paragraph2 = document.createElement('p');
-    const wrapper1 = document.createElement('div');
-    const imageVisible = document.createElement('div');
-    const imageInvisible = document.createElement('div');
-    const Footer = document.createElement('footer');
-    
- 
+  //add/edit attribute
+  //galdinVideo.src= localStorage.theme == 'light'?'https://gta5mod.net/wp-content/uploads/2020/06/Final-Fantasy-XV-Galdin-Quay.jpg':'https://i.ytimg.com/vi/FQcPlk5APAs/maxresdefault.jpg';
+  //imageInvisible.style.backgroundImage = localStorage.theme == 'light' ? `url('https://i.imgur.com/z3PX2uk.png')` :  `url('https://i.imgur.com/gkeuUxp.jpg')`;
+  //imageVisible.style.backgroundImage = localStorage.theme == 'light' ?`url('https://i.imgur.com/enudYqV.jpg')` :  `url('https://i.imgur.com/bnLWCgA.jpg')`;
 
-    //add class
-    Home.classList.toggle('Home');
-    firstSection.classList.toggle('first-section');
-    SecondSection.classList.toggle('second-section');
-    ThirdSection.classList.toggle('third-section');
-    galdinVideo.classList.toggle('galdin-video');
-    firstTextBox.classList.toggle('first-text');
-    movingText.classList.toggle('moving-text');
-    firstWord.classList.toggle('first-word');
-    secondWord.classList.toggle('second-word');
-    logo.classList.toggle('logo2');
-    title3.classList.toggle('title3');
-    wrapper1.classList.toggle('wrapper1');
-    imageVisible.classList.toggle('img-visible');
-    imageInvisible.classList.toggle('img-invisible');
+  logo.src = myLogo;
 
-    //append item
-    Home.appendChild(firstSection);
-    firstSection.appendChild(firstTextBox);
-    firstSection.appendChild(galdinVideo);
-    firstTextBox.appendChild(title1);
-    firstTextBox.appendChild(subTitle1);
-    firstTextBox.appendChild(movingText);
-    movingText.appendChild(firstWord);
-    movingText.appendChild(secondWord);
-    title1.textContent ='The Galdin Quay'
-    subTitle1.textContent ='Ignis Scientia'
-    Home.appendChild(SecondSection);
-    SecondSection.appendChild(logo);
-    SecondSection.appendChild(title2);
-    SecondSection.appendChild(subTitle2);
-    title2.textContent ='16 YEARS'
-    Home.appendChild(ThirdSection);
-    ThirdSection.appendChild(title3);
-    ThirdSection.appendChild(paragraph1);
-    ThirdSection.appendChild(paragraph2);
-    ThirdSection.appendChild(wrapper1);
-    wrapper1.appendChild(imageVisible);
-    imageVisible.appendChild(imageInvisible);
-    subTitle2.textContent ='Final Fantasy XV is an action role-playing game developed and published by Square Enix. The fifteenth main installment of the Final Fantasy series';
-    title3.textContent =' A First-Class Experience';
-    paragraph1.textContent =`A Stunning seaside getaway along the Vannath Coast in southern Leide. Known not only for its world-class spa, but also for the exquisite seafood dishes crafted by Mother of Pearl's chef de cuisine, Coctura Arlund.`;
-    paragraph2.textContent=` With the sea right at our door we offer only the freshest sea food and if your feeling up to it, we'll let you catch you're own fish doubling the experience`;
-    Home.appendChild(Footer);
-    Footer.innerHTML =`<span> Created by  <a href="https://github.com/MijackK/restuarant/tree/main/src"><i class="fab fa-github"></i></a></span>`;
+  const animate = () => {
+    let first = firstWord.childNodes;
+    let second = secondWord.childNodes;
+    first.forEach((element) => {
+      element.style.transform = "translate(0) scale(1)";
+      element.style.opacity = 1;
+    });
+    second.forEach((element) => {
+      element.style.transform = "translate(0) scale(1)";
+      element.style.opacity = 1;
+    });
+  };
 
+  const animatedText = (() => {
+    let animatedText = "WelcomeAdventurer";
+    let displacement = [
+      { x: 133, y: 518 },
+      { x: 327, y: 219 },
+      { x: 465, y: 559 },
+      { x: 473, y: 322 },
+      { x: 317, y: 560 },
+      { x: 512, y: 287 },
+      { x: 675, y: 526 },
+      { x: 41, y: 493 },
+      { x: 209, y: 571 },
+      { x: 331, y: 586 },
+      { x: 363, y: 265 },
+      { x: 540, y: 301 },
+      { x: 462, y: 581 },
+      { x: 633, y: 304 },
+      { x: 522, y: 573 },
+      { x: 675, y: 402 },
+      { x: 663, y: 591 },
+      { x: 716, y: 465 },
+    ];
+    animatedText = animatedText.split("");
 
-    
-    //add/edit attribute
-    //galdinVideo.src= localStorage.theme == 'light'?'https://gta5mod.net/wp-content/uploads/2020/06/Final-Fantasy-XV-Galdin-Quay.jpg':'https://i.ytimg.com/vi/FQcPlk5APAs/maxresdefault.jpg';
-    //imageInvisible.style.backgroundImage = localStorage.theme == 'light' ? `url('https://i.imgur.com/z3PX2uk.png')` :  `url('https://i.imgur.com/gkeuUxp.jpg')`;
-    //imageVisible.style.backgroundImage = localStorage.theme == 'light' ?`url('https://i.imgur.com/enudYqV.jpg')` :  `url('https://i.imgur.com/bnLWCgA.jpg')`;
-  
-    //galdinVideo.src = `https://www.youtube.com/embed/gAKZUce26ao?autoplay=1`;
-   logo.src =myLogo;
- 
-    
-    const animate = () => {
-        let first = firstWord.childNodes;
-        let second = secondWord.childNodes;
-       first.forEach(element => {
-           element.style.transform ='translate(0) scale(1)';
-           element.style.opacity = 1;
-        });
-       second.forEach(element => {
-           element.style.transform ='translate(0) scale(1)';
-           element.style.opacity = 1;
-        });
- 
+    for (let i = 0; i < animatedText.length; i++) {
+      letter.style.transform = `translate(${displacement[i].x}px,${displacement[i].x}px) scale(0.1)`;
+      letter.style.opacity = 0;
+
+      if (i < 7) {
+        letter.textContent = animatedText[i];
+        firstWord.appendChild(letter.cloneNode(true));
+      } else {
+        letter.textContent = animatedText[i];
+        secondWord.appendChild(letter.cloneNode(true));
+      }
     }
- 
-    const animatedText = (() =>{
-        let animatedText = 'WelcomeAdventurer';
-        let displacement = [  {     "x": 133,     "y": 518   },   {     "x": 327,     "y": 219   },   {     "x": 465,     "y": 559   },   {     "x": 473,     "y": 322   },   {     "x": 317,     "y": 560   },   {     "x": 512,     "y": 287   },   {     "x": 675,     "y": 526   },   {     "x": 41,     "y": 493   },   {     "x": 209,     "y": 571   },   {     "x": 331,     "y": 586   },   {     "x": 363,     "y": 265   },   {     "x": 540,     "y": 301   },   {     "x": 462,     "y": 581   },   {     "x": 633,     "y": 304   },   {     "x": 522,     "y": 573   },   {     "x": 675,     "y": 402   },   {     "x": 663,     "y": 591   },   {     "x": 716,     "y": 465   } ];
-        animatedText=animatedText.split('');
+    setTimeout(animate, 300);
+  })();
 
-        for(let i=0; i<animatedText.length; i++){
-            letter.style.transform=`translate(${displacement[i].x}px,${displacement[i].x}px) scale(0.1)`;
-            letter.style.opacity = 0;
-          
-            if( i < 7){
-                letter.textContent = animatedText[i];
-                firstWord.appendChild(letter.cloneNode(true));
-            }
-            else{
-                letter.textContent = animatedText[i];
-                secondWord.appendChild(letter.cloneNode(true));
-            }
-        }
-        setTimeout(animate,300);
-    })();
+  const lookingGlass = (() => {
+    const visible = imageVisible; //document.querySelector('.img-visible');
+    const invisible = imageInvisible; //document.querySelector('.img-invisible');
 
-    const lookingGlass = (() =>{
-        const visible = imageVisible//document.querySelector('.img-visible');
-        const invisible = imageInvisible //document.querySelector('.img-invisible');
+    const moveGlass = (e) => {
+      let coord = visible.getBoundingClientRect();
+      let dimensions = invisible.getBoundingClientRect();
+      let outOfBoundsRight = coord.left + coord.width;
+      let outOfBoundsTop = visible.offsetTop;
+      let outOfBoundsBottom = visible.offsetTop + coord.height;
+      let radius = dimensions.width / 2;
+      invisible.style.opacity = 1;
+      //console.log(`screenX${e.screenX}  clinetX${e.clientX} pageX:${e.pageX} `);
+      //console.log(`imgX:${coord.x} ${coord.left}  imgY: ${coord.y} ${visible.offsetTop}  scrY:${e.screenY} pageY:${e.pageY} cliY:${e.clientY} `);
+      if (
+        coord.left > e.pageX ||
+        e.pageX > outOfBoundsRight ||
+        e.pageY < outOfBoundsTop ||
+        e.pageY > outOfBoundsBottom
+      ) {
+        invisible.style.opacity = 0;
+        return;
+      }
 
-        const moveGlass = (e) =>{
-            let coord = visible.getBoundingClientRect();
-            let dimensions =invisible.getBoundingClientRect();
-            let outOfBoundsRight = coord.left +coord.width;
-            let outOfBoundsTop =visible.offsetTop;
-            let outOfBoundsBottom = visible.offsetTop + coord.height;
-            let radius = dimensions.width/2;
-            invisible.style.opacity=1;
-            //console.log(`screenX${e.screenX}  clinetX${e.clientX} pageX:${e.pageX} `);
-            //console.log(`imgX:${coord.x} ${coord.left}  imgY: ${coord.y} ${visible.offsetTop}  scrY:${e.screenY} pageY:${e.pageY} cliY:${e.clientY} `);
-            if( coord.left > e.pageX || e.pageX > outOfBoundsRight || e.pageY <outOfBoundsTop || e.pageY >outOfBoundsBottom){
-                invisible.style.opacity =0;
-                return
-            }
+      invisible.style.left = `${e.pageX - radius}px`;
+      invisible.style.top = `${e.pageY - radius}px`;
+      //invisible.style.backgroundPosition = `${-1*(e.pageX -coord.x)+radius}px ${-1*(e.pageY - visible.offsetTop)+radius}px` //show without zoom effect, make sure both visible and invisible background size is the same.
+      invisible.style.backgroundPosition = `${
+        -1 * (e.pageX - coord.x) + radius - 25
+      }px ${-1 * (e.pageY - visible.offsetTop) + radius - 25}px`; // invisible background size needs to be larger than visible background size, adjust till desired effect is achieved
+    };
+    visible.addEventListener("mousemove", (e) => moveGlass(e));
+    visible.addEventListener("touchmove", (e) => {
+      e.preventDefault();
+      moveGlass(e.changedTouches[0]);
+    });
+  })();
 
-            invisible.style.left = `${e.pageX - radius}px`;
-            invisible.style.top = `${ e.pageY -radius }px`;
-            //invisible.style.backgroundPosition = `${-1*(e.pageX -coord.x)+radius}px ${-1*(e.pageY - visible.offsetTop)+radius}px` //show without zoom effect, make sure both visible and invisible background size is the same.
-            invisible.style.backgroundPosition = `${-1*(e.pageX -coord.x)+radius -25}px ${-1*(e.pageY - visible.offsetTop)+radius -25}px` // invisible background size needs to be larger than visible background size, adjust till desired effect is achieved
-        }
-        visible.addEventListener('mousemove', e => moveGlass(e));
-        visible.addEventListener('touchmove', e => {
-            e.preventDefault();
-             moveGlass(e.changedTouches[0]);
-        });
-    })();
-
-    return {galdinVideo,imageInvisible,imageVisible}
-   
+  return { galdinVideo, imageInvisible, imageVisible };
 })();
 
-const getHome = () =>{
-    constructHome.galdinVideo.src= localStorage.theme == 'dark'?'https://i.ytimg.com/vi/FQcPlk5APAs/maxresdefault.jpg':'https://gta5mod.net/wp-content/uploads/2020/06/Final-Fantasy-XV-Galdin-Quay.jpg';
-    constructHome.imageInvisible.style.backgroundImage = localStorage.theme == 'dark' ?  `url('https://i.imgur.com/gkeuUxp.jpg')` : `url('https://i.imgur.com/z3PX2uk.png')`;
-    constructHome.imageVisible.style.backgroundImage = localStorage.theme == 'dark' ?  `url('https://i.imgur.com/bnLWCgA.jpg')` : `url('https://i.imgur.com/enudYqV.jpg')`;
-    return Home;
-}
+const getHome = () => {
+  constructHome.galdinVideo.src =
+    localStorage.theme == "dark"
+      ? "https://i.ytimg.com/vi/FQcPlk5APAs/maxresdefault.jpg"
+      : "https://gta5mod.net/wp-content/uploads/2020/06/Final-Fantasy-XV-Galdin-Quay.jpg";
+  constructHome.imageInvisible.style.backgroundImage =
+    localStorage.theme == "dark"
+      ? `url('https://i.imgur.com/gkeuUxp.jpg')`
+      : `url('https://i.imgur.com/z3PX2uk.png')`;
+  constructHome.imageVisible.style.backgroundImage =
+    localStorage.theme == "dark"
+      ? `url('https://i.imgur.com/bnLWCgA.jpg')`
+      : `url('https://i.imgur.com/enudYqV.jpg')`;
+  return Home;
+};
 
-
-
-export{getHome}
+export { getHome };
